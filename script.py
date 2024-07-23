264a0d0e2a811b41a114a3bdb204ddcb09203f95
185662210-myseneca <cduloy@myseneca.ca>
2024-07-23 15:52:06 -0400

    Update script.py

--- script.py
+++ script.py
@@ -1,23 +1,24 @@
 #!/usr/bin/env python3
 
-
 class ProductStock:
-    def _init_(self, name, stock):
+    def __init__(self, name, stock):
         self.name = name
         self.stock = stock
-        self.in_stock = stock > 0
-
+ 
     def make_sale(self, amt):
         self.stock -= amt
         print(f'You take {amt} down, and pass it around')
-        self.in_stock = self.stock > 0
-        
-    def _sub_(self, amt):
-        self.make_sale(amt)
-        return self
     
-    def _repr_(self):
+    @property
+    def in_stock(self):
+        return self.stock > 0
+    
+    def __repr__(self):
         return f'{self.name} {self.stock}x'
     
-    def _str_(self):
-        return f'{self.stock} bottles of {self.name} on the wall, {self.stock} bottles of {self.name}'
+    def __str__(self):
+        return f'{self.stock} bottles of {self.name} on the wall, {self.stock} bottles of {self.name}'
+    
+    def __sub__(self, amt):
+      self.make_sale(amt)
+      return self
